db1f293de69b4b879a995c6d2c4895422eeb853e
Denys Kholod <denyskh@outlook.com>
2016-03-23 14:17:17 +0200

    add service fabric application to host FSI Slack bot instance

---
+++ FsiSlackBotService/FsiSlackBotService.cs
@@ -0,0 +1,25 @@
+﻿namespace FsiSlackBotService
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using BotHost;
+
+    using Microsoft.ServiceFabric.Services.Runtime;
+
+    public class FsiSlackBotService : StatelessService
+    {
+        protected override async Task RunAsync(CancellationToken cancelServiceInstance)
+        {
+            BotBootstrap.initBot();
+
+            var iterations = 0;
+            while (!cancelServiceInstance.IsCancellationRequested)
+            {
+                ServiceEventSource.Current.ServiceMessage(this, "Working-{0}", iterations++);
+                await Task.Delay(TimeSpan.FromSeconds(5), cancelServiceInstance);
+            }
+        }
+    }
+}
